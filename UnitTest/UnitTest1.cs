7f603506d4bfa38c794905b8bc567a33bfa61e6f
rider <jil.p@simformsolutions.com>
2023-06-28 11:34:16 +0530

    add: unit test for test3

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVC_Practical_9.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HomeController homeController = new HomeController();
+
+            ViewResult test3 = homeController.Test3() as ViewResult;
+
+            Assert.AreEqual("Hello World", test3.ViewData["Message"]);
+        }
+    }
+}
